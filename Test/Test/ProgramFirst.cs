b18d783cbac9d65d7ac4bc1272e7abc7be5fff90
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-03-15 19:31:54 +0900

    add ProgramFirst project

---
+++ Test/Test/ProgramFirst.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Name
+{
+    internal class ProgramFirst
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# world!!");
+        }
+    }
+}
