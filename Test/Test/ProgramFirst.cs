26b6eb521408283050b1c8193f106a66084d58f2
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-03-18 21:52:36 +0900

    add Test project

--- Test/Test/ProgramFirst.cs
+++ Test/Test/ProgramFirst.cs
@@ -6,7 +6,12 @@ namespace Name
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello C# world!!");
+            int year = int.Parse(Console.ReadLine());
+            if (year < 1 && year > 4000) return;
+
+            if (DateTime.IsLeapYear(year)) Console.WriteLine(year + "년은 윤년 입니다.");
+            else Console.WriteLine(year + " 년은 윤년이 아닙니다.");
+
         }
     }
 }
