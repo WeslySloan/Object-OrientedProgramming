2b0ae66e498a10bd2eb2ca1b182ab292d1c571db
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-03-23 15:34:50 +0900

    add ppt, new project ( lestt than 10 )

--- Test/Test/ProgramFirst.cs
+++ Test/Test/ProgramFirst.cs
@@ -6,11 +6,30 @@ namespace Name
     {
         static void Main(string[] args)
         {
-            int year = int.Parse(Console.ReadLine());
-            if (year < 1 && year > 4000) return;
+            //int year = int.Parse(Console.ReadLine());
+            //if (year < 1 && year > 4000) return;
 
-            if (DateTime.IsLeapYear(year)) Console.WriteLine(year + "년은 윤년 입니다.");
-            else Console.WriteLine(year + " 년은 윤년이 아닙니다.");
+            //if (DateTime.IsLeapYear(year)) Console.WriteLine(year + "년은 윤년 입니다.");
+            //else Console.WriteLine(year + " 년은 윤년이 아닙니다.");
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        continue;
+            //    Console.WriteLine(i + "");
+            //}
+            //Console.WriteLine();
+
+            // 위의 for 문을 while 문으로 , 1 부터 10까지 짝수만, continue 있어야함
+
+            int i = 0;
+            while ( i < 10 )
+            {
+                i++;
+                if(i % 2 == 0)
+                    continue;
+                Console.WriteLine(i + "");
+            }
 
         }
     }
