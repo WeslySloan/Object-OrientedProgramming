b32c6ca6b42b7ef64668b91becd7bddab63262f0
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-04-01 16:36:39 +0900

    add sumAvg project

---
+++ Test/Class1.cs
@@ -0,0 +1,18 @@
+﻿class Class1
+{
+	static void SumAvg(int[] a)
+	{
+		int sum = 0;
+		for (int = int = 0; int < a.length; int++)
+		{
+			sum += a[i];
+		}
+		Console.Writeline("합계 : " + sum);
+		Console.WriteLine("평균 : " + sum / a.Length);
+
+		static void Main(string[] args)
+		{
+			int[] array = { 1, 2, 3, 4, 5 };
+			SumAvg(array)
+	}
+}
--- Test/Test/ProgramFirst.cs
+++ Test/Test/ProgramFirst.cs
@@ -1,11 +1,46 @@
 ﻿using System;
+//namespace Name
+
+//internal class ProgramnFirst
+//{
+//    static void SumAvg(int[] a)
+//    {
+//        int sum = 0;
+//        for (int i = 0; i < a.Length; i++)
+//        {
+//            sum += a[i];
+//        }
+//        Console.WriteLine("합계 : " + sum);
+//        Console.WriteLine("평균 : " + sum / a.Length);
+
+//        static void Main(string[] args)
+//        {
+//            int[] array = { 1, 2, 3, 4, 5 };
+//            SumAvg(array);
+//        }
+//    }
+//}
 
 namespace Name
 {
     internal class ProgramFirst
     {
+        static void SumAvg(int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            Console.WriteLine("합계 : " + sum);
+            Console.WriteLine("평균 : " + sum / a.Length);
+
+        }
         static void Main(string[] args)
         {
+            int[] array = { 1, 2, 3, 4, 5 };
+            SumAvg(array);
+
             //int year = int.Parse(Console.ReadLine());
             //if (year < 1 && year > 4000) return;
 
@@ -22,15 +57,18 @@ namespace Name
 
             // 위의 for 문을 while 문으로 , 1 부터 10까지 짝수만, continue 있어야함
 
-            int i = 0;
-            while ( i < 10 )
-            {
-                i++;
-                if(i % 2 == 0)
-                    continue;
-                Console.WriteLine(i + "");
-            }
+            //int i = 0;
+            //while (i < 10)
+            //{
+            //    i++;
+            //    if (i % 2 == 0)
+            //        continue;
+            //    Console.WriteLine(i + "");
+            //}
 
         }
     }
 }
+
+
+
