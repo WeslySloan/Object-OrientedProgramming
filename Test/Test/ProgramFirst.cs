12eb187a8941adc2c5a5c10d5537dc89c67204aa
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-04-05 19:45:39 +0900

    update project
    상속 클래스 예제

--- Test/Test/ProgramFirst.cs
+++ Test/Test/ProgramFirst.cs
@@ -23,23 +23,54 @@
 
 namespace Name
 {
+    class GPU
+    {
+        public String MyGPU = "RTX 3080";
+
+        public GPU()
+        {
+            Console.WriteLine("Parent 생성");
+        }
+    }
+
+    class PC : GPU
+    {
+        public PC (String gpu)
+        {
+            this.MyGPU += gpu;
+            Console.WriteLine("gpu 자식 클래스 생성");
+        }
+        
+        public void showGPU()
+        {
+            Console.WriteLine(MyGPU);
+        }
+    }
     internal class ProgramFirst
     {
-        static void SumAvg(int[] a)
-        {
-            int sum = 0;
-            for (int i = 0; i < a.Length; i++)
-            {
-                sum += a[i];
-            }
-            Console.WriteLine("합계 : " + sum);
-            Console.WriteLine("평균 : " + sum / a.Length);
-
-        }
         static void Main(string[] args)
         {
-            int[] array = { 1, 2, 3, 4, 5 };
-            SumAvg(array);
+            PC pc = new PC("RTX3080");
+            pc.showGPU();
+
+            //int num = 6;
+
+            //for (int i = 0; i < num; i++)
+            //{
+            //    for (int j = 0; j < (num - i - 1); j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+            //    for (int k = 0; k <= i; k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    for (int l = 0; l < i; l++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
 
             //int year = int.Parse(Console.ReadLine());
             //if (year < 1 && year > 4000) return;
