59d1818b2e0497045d47684be9f613dbb76a50fd
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-04-21 01:03:00 +0900

    add

--- Test/Test/ProgramFirst.cs
+++ Test/Test/ProgramFirst.cs
@@ -1,160 +1,191 @@
 ﻿using System;
-using System.Collections.Generic;
-namespace Stack
+namespace DivByZeroExceptionApp
 {
-    interface Stack
+    class Program1
     {
-        bool isEmpty();
-        void push(char item);
-        char pop();
-        void delete();
-        char peek();
-    }
-
-    class StackNode
-    {
-        public char data;
-        StackNode link;
-    }
-
-
-    public class LinkedStack : Stack
-    {
-        StackNode top = null;
-
-        public bool isEmpty()
+        public static void Main()
         {
-            return (top == null);
-        }
-
-        public void push(char item)
-        {
-            StackNode newNode = new StackNode();
-            newNode.data = item;
-            newNode.link = top;
-            top = newNode;
-            // System.out.println("Inserted Item : " + item);
-        }
-
-        public char pop()
-        {
-            if (isEmpty())
+            Console.WriteLine("3개의 정수를 입력하세요 : ");
+            int sum = 0, n = 0;
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("Deleting fail! Linked Stack is empty!!");
-                return 0;
-            }
-            else
-            {
-                char item = top.data;
-                top = top.link;
-                return item;
-            }
-        }
-
-        public void delete()
-        {
-            if (isEmpty())
-            {
-                Console.WriteLine("Deleting fail! Linked Stack is empty!!");
-
-            }
-            else
-            {
-                top = top.link;
-            }
-        }
-
-        public char peek()
-        {
-            if (isEmpty())
-            {
-                Console.WriteLine("Peeking fail! Linked Stack is empty!!");
-                return 0;
-            }
-            else
-                return top.data;
-        }
-
-        public void printStack()
-        {
-            if (isEmpty())
-                Console.WriteLine("Linked Stack is empty!! %n %n");
-            else
-            {
-                StackNode temp = top;
-                Console.WriteLine("Linked Stack>> ");
-                while (temp != null)
+                Console.Write(i + " >> ");
+                try
                 {
-                    Console.WriteLine("\t %c \n", temp.data);
-                    temp = temp.link;
+                    n = Convert.ToInt32(Console.ReadLine());
                 }
-                Console.WriteLine();
-            }
-        }
-    }
-
-    public class OptExp
-    {
-        private String exp;
-        private int expSize;
-        private char testCh, openPair;
-
-        public bool testPair(String exp)
-        {
-            this.exp = exp;
-            LinkedStack S = new LinkedStack();
-            expSize = this.exp.length();
-            for (int i = 0; i < expSize; i++)
-            {
-                testCh = this.exp.charAt(i);
-                switch (testCh)
+                catch (FormatException e)
                 {
-                    case '(':
-                    case '{':
-                    case '[':
-                        S.push(testCh);
-                        break;
-                    case ')':
-                    case '}':
-                    case ']':
-                        if (S.isEmpty())
-                            return false;
-                        else
-                        {
-                            openPair = S.pop();
-                            if ((openPair == '(' && testCh != ')') || (openPair == '{' && testCh != '}')
-                                    || (openPair == '[' && testCh != ']'))
-                                return false;
-                            else
-                                break;
-                        }
+                    Console.WriteLine("정수가 아닙니다. 다시 입력해 주세요");
+                    i--;
+                    continue;
                 }
+                sum += n;
             }
-            if (S.isEmpty())
-                return true;
-            else
-                return false;
+            Console.WriteLine("합은 " + sum);
         }
     }
-
-public static class Test
-    {
-        public static void Main(String[] args)
-        {
-            OptExp opt = new OptExp();
-            String exp = "(3*5)-6/2)";
-
-            Console.WriteLine(exp);
-
-            if (opt.testPair(exp))
-                Console.WriteLine("괄호 맞음!!");
-
-            else
-                Console.WriteLine("괄호 틀림!!");
-        }
-    }
-    
 }
+
+
+//using System;
+//using System.Collections.Generic;
+//namespace Stack
+//{
+//    public interface Stack
+//    {
+//        bool isEmpty();
+//        void push(char item);
+//        char pop();
+//        void delete();
+//        char peek();
+//    }
+
+//    public class StackNode
+//    {
+//        public char data;
+//        StackNode link;
+//    }
+
+
+//    public class LinkedStack : Stack
+//    {
+//        StackNode top = null;
+//        //private StackNode top;
+
+//        public bool isEmpty()
+//        {
+//            return (top == null);
+//        }
+
+//        public void push(char item)
+//        {
+//            StackNode newNode = new StackNode();
+//            newNode.data = item;
+//            newNode.link = top;
+//            top = newNode;
+//            // System.out.println("Inserted Item : " + item);
+//        }
+
+//        public char pop()
+//        {
+//            if (isEmpty())
+//            {
+//                Console.WriteLine("Deleting fail! Linked Stack is empty!!");
+//                return 0;
+//            }
+//            else
+//            {
+//                char item = top.data;
+//                top = top.link;
+//                return item;
+//            }
+//        }
+
+//        public void delete()
+//        {
+//            if (isEmpty())
+//            {
+//                Console.WriteLine("Deleting fail! Linked Stack is empty!!");
+
+//            }
+//            else
+//            {
+//                top = top.link;
+//            }
+//        }
+
+//        public char peek()
+//        {
+//            if (isEmpty())
+//            {
+//                Console.WriteLine("Peeking fail! Linked Stack is empty!!");
+//                return 0;
+//            }
+//            else
+//                return top.data;
+//        }
+
+//        public void printStack()
+//        {
+//            if (isEmpty())
+//                Console.WriteLine("Linked Stack is empty!! %n %n");
+//            else
+//            {
+//                StackNode temp = top;
+//                Console.WriteLine("Linked Stack>> ");
+//                while (temp != null)
+//                {
+//                    Console.WriteLine("\t %c \n", temp.data);
+//                    temp = temp.link;
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+
+//    public class OptExp
+//    {
+//        private String exp;
+//        private int expSize;
+//        private char testCh, openPair;
+
+//        public bool testPair(String exp)
+//        {
+//            this.exp = exp;
+//            LinkedStack S = new LinkedStack();
+//            expSize = this.exp.Length;
+//            for (int i = 0; i < expSize; i++)
+//            {
+//                testCh = this.exp.charAt(i);
+//                switch (testCh)
+//                {
+//                    case '(':
+//                    case '{':
+//                    case '[':
+//                        S.push(testCh);
+//                        break;
+//                    case ')':
+//                    case '}':
+//                    case ']':
+//                        if (S.isEmpty())
+//                            return false;
+//                        else
+//                        {
+//                            openPair = S.pop();
+//                            if ((openPair == '(' && testCh != ')') || (openPair == '{' && testCh != '}')
+//                                    || (openPair == '[' && testCh != ']'))
+//                                return false;
+//                            else
+//                                break;
+//                        }
+//                }
+//            }
+//            if (S.isEmpty())
+//                return true;
+//            else
+//                return false;
+//        }
+//    }
+
+//public static class Test
+//    {
+//        public static void Main(String[] args)
+//        {
+//            OptExp opt = new OptExp();
+//            String exp = "(3*5)-6/2)";
+
+//            Console.WriteLine(exp);
+
+//            if (opt.testPair(exp))
+//                Console.WriteLine("괄호 맞음!!");
+
+//            else
+//                Console.WriteLine("괄호 틀림!!");
+//        }
+//    }
+
+//}
 
 //namespace Name
 
