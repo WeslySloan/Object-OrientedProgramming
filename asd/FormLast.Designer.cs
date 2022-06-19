668b7d1a6e2012b8c2c881f1883007771c08ba78
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2022-06-20 04:25:01 +0900

    update oop

---
+++ asd/FormLast.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace lab5work
+{
+    partial class FormLast
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBAnswers = new System.Windows.Forms.Label();
+            this.LBCCode = new System.Windows.Forms.Label();
+            this.LBSem = new System.Windows.Forms.Label();
+            this.LBAns = new System.Windows.Forms.Label();
+            this.LBProf = new System.Windows.Forms.Label();
+            this.LBCCodeL = new System.Windows.Forms.Label();
+            this.LBSemL = new System.Windows.Forms.Label();
+            this.LBProfL = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LBQ3L = new System.Windows.Forms.Label();
+            this.LBQ2L = new System.Windows.Forms.Label();
+            this.LBQ1L = new System.Windows.Forms.Label();
+            this.LBA1L = new System.Windows.Forms.Label();
+            this.LBA2L = new System.Windows.Forms.Label();
+            this.LBA3L = new System.Windows.Forms.Label();
+            this.LBSaveAns = new System.Windows.Forms.Label();
+            this.BTSave = new System.Windows.Forms.Button();
+            this.BTCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LBAnswers
+            // 
+            this.LBAnswers.AutoSize = true;
+            this.LBAnswers.Location = new System.Drawing.Point(34, 29);
+            this.LBAnswers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBAnswers.Name = "LBAnswers";
+            this.LBAnswers.Size = new System.Drawing.Size(89, 12);
+            this.LBAnswers.TabIndex = 0;
+            this.LBAnswers.Text = "Your Answers:";
+            // 
+            // LBCCode
+            // 
+            this.LBCCode.AutoSize = true;
+            this.LBCCode.Location = new System.Drawing.Point(48, 67);
+            this.LBCCode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBCCode.Name = "LBCCode";
+            this.LBCCode.Size = new System.Drawing.Size(84, 12);
+            this.LBCCode.TabIndex = 2;
+            this.LBCCode.Text = "Course Code:";
+            // 
+            // LBSem
+            // 
+            this.LBSem.AutoSize = true;
+            this.LBSem.Location = new System.Drawing.Point(48, 88);
+            this.LBSem.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBSem.Name = "LBSem";
+            this.LBSem.Size = new System.Drawing.Size(63, 12);
+            this.LBSem.TabIndex = 3;
+            this.LBSem.Text = "Semester:";
+            // 
+            // LBAns
+            // 
+            this.LBAns.AutoSize = true;
+            this.LBAns.Location = new System.Drawing.Point(48, 128);
+            this.LBAns.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBAns.Name = "LBAns";
+            this.LBAns.Size = new System.Drawing.Size(59, 12);
+            this.LBAns.TabIndex = 4;
+            this.LBAns.Text = "Answers:";
+            // 
+            // LBProf
+            // 
+            this.LBProf.AutoSize = true;
+            this.LBProf.Location = new System.Drawing.Point(48, 108);
+            this.LBProf.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBProf.Name = "LBProf";
+            this.LBProf.Size = new System.Drawing.Size(63, 12);
+            this.LBProf.TabIndex = 5;
+            this.LBProf.Text = "Professor:";
+            // 
+            // LBCCodeL
+            // 
+            this.LBCCodeL.Location = new System.Drawing.Point(138, 67);
+            this.LBCCodeL.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBCCodeL.Name = "LBCCodeL";
+            this.LBCCodeL.Size = new System.Drawing.Size(99, 20);
+            this.LBCCodeL.TabIndex = 6;
+            // 
+            // LBSemL
+            // 
+            this.LBSemL.Location = new System.Drawing.Point(138, 88);
+            this.LBSemL.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBSemL.Name = "LBSemL";
+            this.LBSemL.Size = new System.Drawing.Size(99, 20);
+            this.LBSemL.TabIndex = 7;
+            // 
+            // LBProfL
+            // 
+            this.LBProfL.Location = new System.Drawing.Point(138, 108);
+            this.LBProfL.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBProfL.Name = "LBProfL";
+            this.LBProfL.Size = new System.Drawing.Size(99, 20);
+            this.LBProfL.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(238, 144);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 20);
+            this.label4.TabIndex = 9;
+            // 
+            // LBQ3L
+            // 
+            this.LBQ3L.AutoSize = true;
+            this.LBQ3L.Location = new System.Drawing.Point(48, 193);
+            this.LBQ3L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBQ3L.Name = "LBQ3L";
+            this.LBQ3L.Size = new System.Drawing.Size(353, 12);
+            this.LBQ3L.TabIndex = 10;
+            this.LBQ3L.Text = "Professor demonstrates a thorough knowledge of the subject:";
+            // 
+            // LBQ2L
+            // 
+            this.LBQ2L.Location = new System.Drawing.Point(48, 173);
+            this.LBQ2L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBQ2L.Name = "LBQ2L";
+            this.LBQ2L.Size = new System.Drawing.Size(308, 20);
+            this.LBQ2L.TabIndex = 11;
+            this.LBQ2L.Text = "Overall, I would rate this professor’s performance as:";
+            // 
+            // LBQ1L
+            // 
+            this.LBQ1L.AutoSize = true;
+            this.LBQ1L.Location = new System.Drawing.Point(48, 152);
+            this.LBQ1L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBQ1L.Name = "LBQ1L";
+            this.LBQ1L.Size = new System.Drawing.Size(283, 12);
+            this.LBQ1L.TabIndex = 12;
+            this.LBQ1L.Text = "Overall, I would rate the quality of this course as:";
+            // 
+            // LBA1L
+            // 
+            this.LBA1L.Location = new System.Drawing.Point(329, 152);
+            this.LBA1L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBA1L.Name = "LBA1L";
+            this.LBA1L.Size = new System.Drawing.Size(99, 20);
+            this.LBA1L.TabIndex = 13;
+            // 
+            // LBA2L
+            // 
+            this.LBA2L.Location = new System.Drawing.Point(350, 173);
+            this.LBA2L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBA2L.Name = "LBA2L";
+            this.LBA2L.Size = new System.Drawing.Size(99, 20);
+            this.LBA2L.TabIndex = 14;
+            // 
+            // LBA3L
+            // 
+            this.LBA3L.Location = new System.Drawing.Point(393, 193);
+            this.LBA3L.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBA3L.Name = "LBA3L";
+            this.LBA3L.Size = new System.Drawing.Size(128, 20);
+            this.LBA3L.TabIndex = 15;
+            // 
+            // LBSaveAns
+            // 
+            this.LBSaveAns.AutoSize = true;
+            this.LBSaveAns.Location = new System.Drawing.Point(138, 234);
+            this.LBSaveAns.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.LBSaveAns.Name = "LBSaveAns";
+            this.LBSaveAns.Size = new System.Drawing.Size(255, 12);
+            this.LBSaveAns.TabIndex = 16;
+            this.LBSaveAns.Text = "Do you want to proceed and save answers?";
+            // 
+            // BTSave
+            // 
+            this.BTSave.Location = new System.Drawing.Point(149, 258);
+            this.BTSave.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.BTSave.Name = "BTSave";
+            this.BTSave.Size = new System.Drawing.Size(88, 21);
+            this.BTSave.TabIndex = 17;
+            this.BTSave.Text = "Save";
+            this.BTSave.UseVisualStyleBackColor = true;
+            this.BTSave.Click += new System.EventHandler(this.BTSave_Click);
+            // 
+            // BTCancel
+            // 
+            this.BTCancel.Location = new System.Drawing.Point(284, 258);
+            this.BTCancel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.BTCancel.Name = "BTCancel";
+            this.BTCancel.Size = new System.Drawing.Size(88, 21);
+            this.BTCancel.TabIndex = 18;
+            this.BTCancel.Text = "Cancel";
+            this.BTCancel.UseVisualStyleBackColor = true;
+            this.BTCancel.Click += new System.EventHandler(this.BTCancel_Click);
+            // 
+            // FormLast
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 294);
+            this.Controls.Add(this.BTCancel);
+            this.Controls.Add(this.BTSave);
+            this.Controls.Add(this.LBSaveAns);
+            this.Controls.Add(this.LBA3L);
+            this.Controls.Add(this.LBA2L);
+            this.Controls.Add(this.LBA1L);
+            this.Controls.Add(this.LBQ1L);
+            this.Controls.Add(this.LBQ2L);
+            this.Controls.Add(this.LBQ3L);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.LBProfL);
+            this.Controls.Add(this.LBSemL);
+            this.Controls.Add(this.LBCCodeL);
+            this.Controls.Add(this.LBProf);
+            this.Controls.Add(this.LBAns);
+            this.Controls.Add(this.LBSem);
+            this.Controls.Add(this.LBCCode);
+            this.Controls.Add(this.LBAnswers);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FormLast";
+            this.Text = "Last";
+            this.Load += new System.EventHandler(this.FormLast_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBAnswers;
+        //private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        //private Microsoft.VisualBasic.PowerPacks.RectangleShape rectangleShape1;
+        private System.Windows.Forms.Label LBCCode;
+        private System.Windows.Forms.Label LBSem;
+        private System.Windows.Forms.Label LBAns;
+        private System.Windows.Forms.Label LBProf;
+        private System.Windows.Forms.Label LBCCodeL;
+        private System.Windows.Forms.Label LBSemL;
+        private System.Windows.Forms.Label LBProfL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LBQ3L;
+        private System.Windows.Forms.Label LBQ2L;
+        private System.Windows.Forms.Label LBQ1L;
+        private System.Windows.Forms.Label LBA1L;
+        private System.Windows.Forms.Label LBA2L;
+        private System.Windows.Forms.Label LBA3L;
+        private System.Windows.Forms.Label LBSaveAns;
+        private System.Windows.Forms.Button BTSave;
+        private System.Windows.Forms.Button BTCancel;
+    }
+}
